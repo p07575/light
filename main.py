fd44e53b7af58bed7f974e51c480de19117e6df4
項敬軒 <77497570+p07575@users.noreply.github.com>
2022-05-12 12:19:51 +0800

    MakeCode 專案的初始檔案

---
+++ main.py
@@ -0,0 +1,4 @@
+while True:
+    lightness=0
+    if lightness <= 30:
+        pins.digital_write_pin(DigitalPin.P0, 1)
---
+++ main.ts
@@ -0,0 +1,8 @@
+let lightness: number;
+while (true) {
+    lightness = 0
+    if (lightness <= 30) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+    
+}
